c58d52bd8b47801e0af03e79f675e8a3837149ba
unknown <aeryan@uci.edu>
2021-08-11 23:33:03 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Validates form input; ensures that user inputs all necessary information correctly before calculating results. Deploys error messages instructing user to make corrections where necessary.
+// Fires when user clicks submit form. 
+
+function _validateForm() {
+    return;
+};
+
+// Calculates results; takes user information from form input, and calculates the user's BMR and TDEE. Uses the appropriate formulas/modifiers based on gender and activity level. 
+// Fires when user clicks submit form, and inputs are determined valid based on validateForm(). 
+
+function _calculateResults() {
+    return;
+}
+
+// Displays results; sends the BMR and TDEE output from calculateResults() back to the user. 
+// Fires when user clicks submit form, and inputs are determined valid based on validateForm(). 
+
+function displayResults () {
+    return;
+}
